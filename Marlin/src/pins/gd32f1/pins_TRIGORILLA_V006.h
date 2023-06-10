3a34672fce17c649be2e6110107bd120d0261aa8
Roi Rodriguez Huertas <roiamorin69@gmail.com>
2023-06-10 14:11:21 +0200

    fix fan def

--- Marlin/src/pins/gd32f1/pins_TRIGORILLA_V006.h
+++ Marlin/src/pins/gd32f1/pins_TRIGORILLA_V006.h
@@ -90,7 +90,7 @@
 //
 #define FAN0_PIN                            PA0   // FAN
 #define FAN1_PIN                            PA14  // Connected to +24V
-#define FAN2_PIN                            -1    // PB1, auto fan for E0
+#define FAN2_PIN                            PB1   //, auto fan for E0
 #define CONTROLLER_FAN_PIN              FAN1_PIN
 
 //
